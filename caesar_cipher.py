e2cbbf0abc114bb1cbd0606deb655e26481668a6
Enitanmayomi <166308997+Enitanmayomi@users.noreply.github.com>
2024-11-14 05:27:17 -0800

    Add files via upload

---
+++ caesar_cipher.py
@@ -0,0 +1,23 @@
+def caesar_cipher(text, shift):
+    result = ""
+    for char in text:
+        if char.isalpha():
+            shift_char = ord(char) + shift
+            if char.isupper():
+                shift_char = shift_char % 65 + 65
+            else:
+                shift_char = shift_char % 97 + 97
+            result += chr(shift_char)
+        else:
+            result += char
+    return result
+
+def main():
+    text = input("Enter your message: ")
+    shift = int(input("Enter the shift value: "))
+
+    encrypted_text = caesar_cipher(text, shift)
+    print("Encrypted text:", encrypted_text)
+
+if __name__ == "__main__":
+    main()
